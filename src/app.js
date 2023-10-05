5422d70e8b69383c7dbfe2530235eb27a6955b70
daredevil17052004 <anshs052004@gmail.com>
2023-10-05 09:29:32 +0530

    updatedFile

--- src/app.js
+++ src/app.js
@@ -26,7 +26,24 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    for(let i = 0; i < players.length ; i++){
+        let detailedPlayers = [];
+        
+        if (i%2 ==0){
+            type = "villain";
+        }else {
+            type = 'hero';
+        }
 
+        let details = {
+            name : players[i],
+            strength : getRandomStrength(),
+            Image : "images/super-"+(i+1)+".png",
+            type,
+        }
+        detailedPlayers.push(details);
+        
+    }
     return detailedPlayers;
 }
 
@@ -34,7 +51,10 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    let x = Math.round(Math.floor(Math.random()*100));
+    return x;
 }
+getRandomStrength()
 
 const buildPlayers = (players, type) => {
     let fragment = '';
@@ -42,7 +62,15 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
+     for( let i = 1; i < 100; i++){
+         if(players[i].type === type){
+             fragment += `<div class="players">
+             <img src="${players.image}" alt="">
+             <div class="name">${players .name}</div>
+             <div class="strength">${players.strength}</div>
+            </div>`;
+         }
+    }
     return fragment;
 }
 // Display players in HTML
